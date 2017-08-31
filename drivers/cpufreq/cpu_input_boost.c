c02119c341aaffcad3c9467225971c083c5e8de2
Joe Maples <joe@frap129.org>
2017-08-31 14:12:28 -0400

    cpu_input_boost: Consider fingerprint sensor input
    Signed-off-by: Joe Maples <joe@frap129.org>
    Signed-off-by: AyushR1 <ayushrathore12501@gmail.com>

--- drivers/cpufreq/cpu_input_boost.c
+++ drivers/cpufreq/cpu_input_boost.c
@@ -1,7 +1,6 @@
 /*
  * Copyright (C) 2014-2017, Sultanxda <sultanxda@gmail.com>
  *           (C) 2017, Joe Maples <joe@frap129.org>
- *           (C) 2017, Wang Han <416810799@qq.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
